ff237fb6af67892b1e9e4dd596d1cb579fc007d3
@hexlet/cli <support@hexlet.io>
2023-05-29 15:06:13 +0200

    submit java-collections-ru/maps

--- java-collections-ru/maps/src/main/java/exercise/App.java
+++ java-collections-ru/maps/src/main/java/exercise/App.java
@@ -3,6 +3,14 @@ package exercise;
 import java.util.HashMap;
 import java.util.Map;
 
+public class App {
+    public static void main(String[] args) {
+        switch ("1") {
+            case "1" -> System.out.println(1);
+            case "22" -> System.out.println(2);
+        }
+    }
+}
 // BEGIN
 
 //END
